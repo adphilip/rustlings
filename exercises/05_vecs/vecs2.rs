19fc7995d2a1f79cdf3ea1edafae3583407855a8
Adrian Filpescu <adrian.filipescu@gmail.com>
2025-12-29 09:07:05 +0200

    vectors

--- exercises/05_vecs/vecs2.rs
+++ exercises/05_vecs/vecs2.rs
@@ -4,6 +4,8 @@ fn vec_loop(input: &[i32]) -> Vec<i32> {
     for element in input {
         // TODO: Multiply each element in the `input` slice by 2 and push it to
         // the `output` vector.
+        output.push(element * 2);
+
     }
 
     output
@@ -11,6 +13,8 @@ fn vec_loop(input: &[i32]) -> Vec<i32> {
 
 fn main() {
     // You can optionally experiment here.
+   let output=  vec_loop(&[2, 4, 6, 8, 10]);
+    println!("See tests for validation!{} ", output[0]);
 }
 
 #[cfg(test)]

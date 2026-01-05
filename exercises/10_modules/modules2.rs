11fac03cd8996140bfdc584a304dda62eb3a3c56
Adrian Filpescu <adrian.filipescu@gmail.com>
2026-01-05 14:21:52 +0200

    modules

--- exercises/10_modules/modules2.rs
+++ exercises/10_modules/modules2.rs
@@ -3,24 +3,27 @@
 
 mod delicious_snacks {
     // TODO: Add the following two `use` statements after fixing them.
-    // use self::fruits::PEAR as ???;
-    // use self::veggies::CUCUMBER as ???;
 
-    mod fruits {
+
+    pub mod fruits {
         pub const PEAR: &str = "Pear";
         pub const APPLE: &str = "Apple";
     }
 
-    mod veggies {
+    pub mod veggies {
         pub const CUCUMBER: &str = "Cucumber";
         pub const CARROT: &str = "Carrot";
     }
 }
 
+    use self::delicious_snacks::fruits::PEAR as PEAR_FRUIT;
+    use self::delicious_snacks::veggies::CUCUMBER as CUCUMBER_VEGGIE;
+    
 fn main() {
+
     println!(
         "favorite snacks: {} and {}",
-        delicious_snacks::fruit,
-        delicious_snacks::veggie,
+        PEAR_FRUIT,
+        CUCUMBER_VEGGIE,
     );
 }
